b4d3558c097ce87f882dd28c72666200c084898d
Justin Bell <planbmanjb@gmail.com>
2017-10-09 10:55:18 -0400

    version 10-09-2017

--- app.js
+++ app.js
@@ -6,13 +6,16 @@ const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
 $(document).ready(function () {
     //    $('.js-query').val("");
     // add comments about what the variables will be used for
-    var searchText = "";
+    //    var searchText = "";
+    //    var TheHtmlOutputtitle = "";
     var searchTerm = [];
     var resultdata = [];
     //on search all this function (Famous Person)
     $('.js-search-form').submit(function () {
         event.preventDefault();
         $('.youtube-videos').empty();
+        //        $('.wiki-name').empty();
+
         var searchText = $('.js-query').val();
 
         // If input field empty, display error message and hide yahootube and wiki sections
@@ -42,16 +45,24 @@ function youtubeApiCall(searchTerm) {
             $('.youtube-videos').html("");
             console.log(resultdata);
             console.log(resultdata.items.length);
-            // search for video results in data object and search for video id. Build iframe and append to youtube video section
-            for (i = 0; i < resultdata.items.length; i++) {
-                console.log(resultdata.items[0].id.videoId)
-                var eachVideoId = resultdata.items[i].id.videoId
+            if (resultdata.pageInfo.totalResults == 0) {
+                alert("No videos");
+                //                var noResponse = "<p> No videos </p>";
+                //                $('.noResponse').show();
+                //                $('.youtube-section').hide();
+            } else {
+                //                $('.noResponse').hide();
+                // search for video results in data object and search for video id. Build iframe and append to youtube video section
+                for (i = 0; i < resultdata.items.length; i++) {
+                    console.log(resultdata.items[0].id.videoId)
+                    var eachVideoId = resultdata.items[i].id.videoId
 
-                var videoResults = '<iframe class="yahooclass"' + 'src = ' + "https://www.youtube.com/embed/" + eachVideoId + '></iframe>'
+                    var videoResults = '<iframe class="yahooclass"' + 'src = ' + "https://www.youtube.com/embed/" + eachVideoId + '></iframe>'
 
-                console.log(videoResults);
-                $('.youtube-videos').append(videoResults);
-                $('.youtube-section').show();
+                    console.log(videoResults);
+                    $('.youtube-videos').append(videoResults);
+                    $('.youtube-section').show();
+                }
             }
         }
     )
@@ -70,27 +81,39 @@ function wikiApiCall(searchTerm) {
         //   callback function to display Wikidata object
         .done(function (Wikidata) {
             var buildTheHtmlOutput = "";
+            //            console.log(Wikidata);
             // looping through the Wikidata.query.pages array, we are using the Wikikey as the index and Wikivalue as the value of the index
+            var TheHtmlOutputtitle = "";
+            var TheHtmlOutputpic = "";
+            var TheHtmlOutputextract = "";
             $.each(Wikidata.query.pages, function (Wikikey, Wikivalue) {
                 // if the Wikivalue.thumbnail and Wikivalue.title are equal to our searchTerm, then display result
-                if (Wikivalue.thumbnail && Wikivalue.title == searchTerm) {
-                    // console.log(Wikivalue.title);
+                console.log(searchTerm);
+                //if (Wikivalue.thumbnail && Wikivalue.title == searchTerm) {
+                console.log(Wikivalue.title);
+                if (Wikivalue.title.toLowerCase() == searchTerm.toLowerCase()) {
+                    $('.noResponse').hide();
                     console.log(Wikivalue.thumbnail.source);
                     // console.log(Wikivalue.extract);
                     console.log(Wikivalue);
 
 
-                    TheHtmlOutputtitle = "<p>" + Wikivalue.title + "</p>";
+                    TheHtmlOutputtitle += "<p>" + Wikivalue.title + "</p>";
+                    console.log(TheHtmlOutputtitle);
                     // Set image equal to TheHtmlOutputpic
-                    TheHtmlOutputpic = '<img style = "height: 75px;" ' + 'src="' + Wikivalue.thumbnail.source + '" />';
+                    TheHtmlOutputpic += '<img style = "height: 75px;" ' + 'src="' + Wikivalue.thumbnail.source + '" />';
                     // Set wikipedia text equal to wikivalue.extract
-                    TheHtmlOutputextract = "<p>" + Wikivalue.extract + "</p>";
-                } else {}
+                    TheHtmlOutputextract += "<p>" + Wikivalue.extract + "</p>";
+                } else {
+                    alert("No wiki");
+                    //                    var noResponse = "<p> No Wiki results </p>";
+                    //                    $('.noResponse').show();
+                }
             })
             // Show wikidata-section with persons name (wiki-name), persons picture (wiki-picture) and persons bio (wiki-extract)
             $('.wikidata-section').show();
-            $(".wiki-name").html(TheHtmlOutputtitle);
-            $(".wiki-picture").html(TheHtmlOutputpic);
+            $('.wiki-name').html(TheHtmlOutputtitle);
+            $('.wiki-picture').html(TheHtmlOutputpic);
             $(".wiki-extract").html(TheHtmlOutputextract);
 
         })
