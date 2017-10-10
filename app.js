4068b7e6b9e4741e6aab6cd37cb68b770b09318f
Justin Bell <planbmanjb@gmail.com>
2017-10-09 21:29:15 -0400

    resolved issue with bad input and incorrect error message

--- app.js
+++ app.js
@@ -45,11 +45,12 @@ function youtubeApiCall(searchTerm) {
             $('.youtube-videos').html("");
             console.log(resultdata);
             console.log(resultdata.items.length);
+
             if (resultdata.pageInfo.totalResults == 0) {
-                alert("No videos");
+                //                alert("No videos");
                 //                var noResponse = "<p> No videos </p>";
-                //                $('.noResponse').show();
-                //                $('.youtube-section').hide();
+                $('.noResponse').show();
+                $('.youtube-section').hide();
             } else {
                 //                $('.noResponse').hide();
                 // search for video results in data object and search for video id. Build iframe and append to youtube video section
@@ -81,40 +82,54 @@ function wikiApiCall(searchTerm) {
         //   callback function to display Wikidata object
         .done(function (Wikidata) {
             var buildTheHtmlOutput = "";
-            //            console.log(Wikidata);
-            // looping through the Wikidata.query.pages array, we are using the Wikikey as the index and Wikivalue as the value of the index
-            var TheHtmlOutputtitle = "";
-            var TheHtmlOutputpic = "";
-            var TheHtmlOutputextract = "";
-            $.each(Wikidata.query.pages, function (Wikikey, Wikivalue) {
-                // if the Wikivalue.thumbnail and Wikivalue.title are equal to our searchTerm, then display result
-                console.log(searchTerm);
-                //if (Wikivalue.thumbnail && Wikivalue.title == searchTerm) {
-                console.log(Wikivalue.title);
-                if (Wikivalue.title.toLowerCase() == searchTerm.toLowerCase()) {
-                    $('.noResponse').hide();
-                    console.log(Wikivalue.thumbnail.source);
-                    // console.log(Wikivalue.extract);
-                    console.log(Wikivalue);
+            console.log(Wikidata);
+            // if the input is junk then tell them to enter a famous person
+            if (typeof Wikidata.query !== 'undefined') {
 
+                // looping through the Wikidata.query.pages array, we are using the Wikikey as the index and Wikivalue as the value of the index
+                var TheHtmlOutputtitle = "";
+                var TheHtmlOutputpic = "";
+                var TheHtmlOutputextract = "";
+                //            console.log(Wikidata.query.items.length);
+                //            if (Wikidata.query.pages == 0) {
+                //                alert("Wikidata.query.pages = 0");
+                //            }
+                $.each(Wikidata.query.pages, function (Wikikey, Wikivalue) {
+                    // if the Wikivalue.thumbnail and Wikivalue.title are equal to our searchTerm, then display result
+                    console.log(searchTerm);
+                    //if (Wikivalue.thumbnail && Wikivalue.title == searchTerm) {
+                    console.log(Wikivalue.title);
+                    console.log(Wikivalue.title.toLowerCase());
+                    var countTitle = 0;
+                    if (Wikivalue.title.toLowerCase() == searchTerm.toLowerCase()) {
+                        $('.noResponse').hide();
+                        //                    console.log(Wikivalue.thumbnail.source);
+                        // console.log(Wikivalue.extract);
+                        //                    console.log(Wikivalue);
 
-                    TheHtmlOutputtitle += "<p>" + Wikivalue.title + "</p>";
-                    console.log(TheHtmlOutputtitle);
-                    // Set image equal to TheHtmlOutputpic
-                    TheHtmlOutputpic += '<img style = "height: 75px;" ' + 'src="' + Wikivalue.thumbnail.source + '" />';
-                    // Set wikipedia text equal to wikivalue.extract
-                    TheHtmlOutputextract += "<p>" + Wikivalue.extract + "</p>";
-                } else {
-                    alert("No wiki");
-                    //                    var noResponse = "<p> No Wiki results </p>";
-                    //                    $('.noResponse').show();
-                }
-            })
-            // Show wikidata-section with persons name (wiki-name), persons picture (wiki-picture) and persons bio (wiki-extract)
-            $('.wikidata-section').show();
-            $('.wiki-name').html(TheHtmlOutputtitle);
-            $('.wiki-picture').html(TheHtmlOutputpic);
-            $(".wiki-extract").html(TheHtmlOutputextract);
+                        TheHtmlOutputtitle += "<p>" + Wikivalue.title + "</p>";
+                        console.log(TheHtmlOutputtitle);
+                        // Set image equal to TheHtmlOutputpic
+                        TheHtmlOutputpic += '<img style = "height: 75px;" ' + 'src="' + Wikivalue.thumbnail.source + '" />';
+                        // Set wikipedia text equal to wikivalue.extract
+                        TheHtmlOutputextract += "<p>" + Wikivalue.extract + "</p>";
+                    } else {
+                        //                        alert("No wiki");
+                        //                    var noResponse = "<p> No Wiki results </p>";
+                        //                        $('.noResponse').show();
+                    }
+                })
+
+                // Show wikidata-section with persons name (wiki-name), persons picture (wiki-picture) and persons bio (wiki-extract)
+                $('.wikidata-section').show();
+                $('.wiki-name').html(TheHtmlOutputtitle);
+                $('.wiki-picture').html(TheHtmlOutputpic);
+                $(".wiki-extract").html(TheHtmlOutputextract);
+            } else {
+                //                alert("Wikidata.query.pages is undefined");
+                $('.noResponse').show();
+                $('.wikidata-section').hide();
+            }
 
         })
         // Unsucessful call to API
